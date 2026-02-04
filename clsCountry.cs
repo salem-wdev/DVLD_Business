11de3997628e1c9e5d7241b234a6753b89037bc2
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-02-04 17:53:43 +0300

    Added Class clsCountry

---
+++ clsCountry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLD_DataAccess;
+
+namespace DVLD_Business
+{
+    public class clsCountry
+    {
+        public static DataTable GetAllCountries()
+        {
+            return clsCountryData.GetAllCountries();
+        }
+    }
+}
